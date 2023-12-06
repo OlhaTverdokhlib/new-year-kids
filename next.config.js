7c5c2e7da5e173762c549f2f072fbe1dcaf06eb5
Olha Tverdokhlib <balabolya@gmail.com>
2023-12-06 20:22:06 +0100

    next.config updated

--- next.config.js
+++ next.config.js
@@ -1,27 +1,5 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-// const nextConfig = {
-//   trailingSlash: true,
-//   output: "export",
+const nextConfig = {
 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  output: 'export', 
 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
-
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-// };
-
-module.exports = {
-  // Інші налаштування...
-  // Додайте або оновіть наступне поле для налаштування експорту
-  output: {
-    // Налаштування для експорту
-    // Встановіть export на true, щоб ввімкнути експорт статичного контенту
-    export: true,
-  },
 };
-
